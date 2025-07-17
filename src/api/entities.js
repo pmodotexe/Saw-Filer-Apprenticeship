44f37d59b82dff99794c3651c2a8b45cc30e8866
pmodotexe <paolomorales@reliabilitysolutions.net>
2025-07-17 19:25:43 -0400

    Add files via upload

---
+++ src/api/entities.js
@@ -0,0 +1,19 @@
+import { base44 } from './base44Client';
+
+
+export const PortalContent = base44.entities.PortalContent;
+
+export const ApprenticeUser = base44.entities.ApprenticeUser;
+
+export const ChatMessage = base44.entities.ChatMessage;
+
+export const UserActivity = base44.entities.UserActivity;
+
+export const Cohort = base44.entities.Cohort;
+
+export const UserNotification = base44.entities.UserNotification;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ src/api/functions.js
@@ -0,0 +1,7 @@
+import { base44 } from './base44Client';
+
+
+export const sendVerificationEmail = base44.functions.sendVerificationEmail;
+
+export const bulkDeleteUsers = base44.functions.bulkDeleteUsers;
+
